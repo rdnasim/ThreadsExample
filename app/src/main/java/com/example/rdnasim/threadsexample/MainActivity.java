550bc7cdf2750422370f503e4154b6033ecf3e5f
rdnasim <rdislamnasim78@gmail.com>
2017-05-15 10:33:49 +0600

    Updated

--- app/src/main/java/com/example/rdnasim/threadsexample/MainActivity.java
+++ app/src/main/java/com/example/rdnasim/threadsexample/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.rdnasim.threadsexample;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,6 +14,16 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void ClickHeroAlam(View view) {
-
+        long futuretime = System.currentTimeMillis() + 5000;
+        while (System.currentTimeMillis() < futuretime) {
+            synchronized (this) {
+                try {
+                    wait(futuretime - System.currentTimeMillis());
+                } catch (Exception e) {
+                }
+            }
+        }
+        TextView myText = (TextView) findViewById(R.id.myText);
+        myText.setText("Please Search in\nwww.Google.com.bd");
     }
 }
